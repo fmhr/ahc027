5c9fbab0b5b417590511803ef8b8f72fda60698b
fmhr <fmhr29@gmail.com>
2023-12-02 11:02:26 +0900

    add gridViewer

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 )
@@ -10,14 +11,15 @@ func main() {
 	readInput()
 }
 
+var N int
+var hWall [40][40]bool
+var vWall [40][40]bool
+
 func readInput() {
-	var N int
 	_, err := fmt.Scan(&N)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var h [40][40]bool
-	var v [40][40]bool
 	var d [40][40]int
 	for i := 0; i < N-1; i++ {
 		var s string
@@ -27,7 +29,7 @@ func readInput() {
 		}
 		for j := 0; j < N; j++ {
 			if s[j] == '1' {
-				h[i][j] = true
+				hWall[i][j] = true
 			}
 		}
 	}
@@ -39,12 +41,12 @@ func readInput() {
 		}
 		for j := 0; j < N-1; j++ {
 			if s[j] == '1' {
-				v[i][j] = true
+				vWall[i][j] = true
 			}
 		}
 	}
 	for i := 0; i < N; i++ {
-		for j := 0; j < N-1; j++ {
+		for j := 0; j < N; j++ {
 			_, err := fmt.Scan(&d[i][j])
 			if err != nil {
 				log.Fatal(err)
@@ -52,14 +54,40 @@ func readInput() {
 		}
 	}
 
-	log.Println(N)
-	for i := 0; i < N; i++ {
-		log.Println(h[i])
+	gridView(d)
+}
+
+func gridView(grid [40][40]int) {
+	var buffer bytes.Buffer
+	buffer.WriteString("\n")
+	for i := 0; i <= 2*N; i++ {
+		for j := 0; j <= 2*N; j++ {
+			switch {
+			case i%2 == 0 && j%2 == 0:
+				buffer.WriteString("+")
+			case i == 0 || i == 2*N:
+				buffer.WriteString("---")
+			case j == 0 || j == 2*N:
+				buffer.WriteString("|")
+			case i%2 == 0:
+				if hWall[i/2-1][(j-1)/2] {
+					buffer.WriteString("---")
+				} else {
+					buffer.WriteString("   ")
+				}
+			case j%2 == 0:
+				if vWall[(i-1)/2][j/2-1] {
+					buffer.WriteString("|")
+				} else {
+					buffer.WriteString(" ")
+				}
+			default:
+				y := (i - 1) / 2
+				x := (j - 1) / 2
+				buffer.WriteString(fmt.Sprintf("%3d", grid[y][x]))
+			}
+		}
+		buffer.WriteString("\n")
 	}
-	for j := 0; j < N; j++ {
-		log.Println(v[j])
-	}
-	for k := 0; k < N; k++ {
-		log.Println(d[k][0 : N-1])
-	}
+	fmt.Print(buffer.String())
 }
